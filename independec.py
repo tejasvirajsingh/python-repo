32e2a039f53d5aca3631e94b798ffd5d8e695775
Tejasvi raj singh <168378423+tejasvirajsingh@users.noreply.github.com>
2025-08-21 23:47:43 +0530

    Modified names independec.py

--- independec.py
+++ independec.py
@@ -3,7 +3,7 @@ print("hello")
 def display_facts():
     facts = [
         "India gained independence from British rule on August 15, 1947.",
-        "The Indian Independence Movement was led by leaders like Mahatma Gandhi, Jawaharlal Nehru, and Sardar Vallabhbhai Patel.",
+        "The Indian Independence Movement was led by leaders like Chandrashekhar Azad, Veer Bhagat Singh, and Sardar Vallabhbhai Patel.",
         "On August 15, 1947, Jawaharlal Nehru, the first Prime Minister of India, raised the Indian national flag at the Red Fort in Delhi.",
         "The Independence Day is celebrated with flag hoisting ceremonies, parades, and cultural events across India.",
         "The Indian national flag was designed by Pingali Venkayya and adopted on July 22, 1947."
